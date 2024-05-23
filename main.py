8008f70d03f0516d704ce7773b63ea7d7ae629ba
Michal-Ploszczyca <michal.ploszczyca98@gmail.com>
2024-05-23 21:35:15 +0200

    report added

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    get_num_letters(text)
 
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
 
-def count_words(file_contents):
-    words = file_contents.split()
-    # print(words)
-    counter = 0
-    print(len(words))
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
-count_words(file_contents)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_letters(text):
+    text = text.lower()
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    sort_char_count = list(char_count.values())
+    sort_char_count.sort()
+    dict(sort_char_count)
+    for key, value in sort_char_count():
+        print(f"The '{key}' character was found {value} times")
+    print("--- End report ---")
+    return char_count
+    
+
+    # print(to_lower)
+
+main()
